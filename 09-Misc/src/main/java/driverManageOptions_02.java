c63a30a13bff6765c6f578e20e2cba3d39de1187
BuzzHeng <leeyuanheng16@gmail.com>
2025-05-21 17:10:03 +0800

    Added code to take screenshot

--- 09-Misc/src/main/java/driverManageOptions_02.java
+++ 09-Misc/src/main/java/driverManageOptions_02.java
@@ -1,8 +1,14 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+
 public class driverManageOptions_02 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
@@ -10,5 +16,8 @@ public class driverManageOptions_02 {
         // Click on any link
         // Login page verify login url
         driver.get("https://google.com");
+
+        File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src, new File("C:\\Users\\leeyu\\Pictures\\Screenshots\\screenshot.png"));
     }
 }
