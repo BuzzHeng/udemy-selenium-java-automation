f82a67d32f2e248eaec38fd798d35bb1e744994a
Yuan Heng Lee <leeyuanheng16@gmail.com>
2025-06-03 16:59:13 +0800

    Added StandAloneTest01 and StandAloneTest02

---
+++ 14-FrameWork/src/test/java/rahulshettyacademy/StandAloneTest01.java
@@ -0,0 +1,53 @@
+package rahulshettyacademy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class StandAloneTest01 {
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://rahulshettyacademy.com/client");
+
+        String username = "scrashers@gmail.com";
+        String password = "@QWE12345qwe";
+
+        driver.findElement(By.id("userEmail")).sendKeys(username);
+        driver.findElement(By.id("userPassword")).sendKeys(password);
+        driver.findElement(By.id("login")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+
+        List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+
+        /* Normal Loops
+        for(int i =0; i<products.size();i++){
+            String getName = products.get(i).findElement(By.cssSelector("h5 b")).getText();
+            System.out.println(getName);
+        }
+        */
+        // Latest way of writing code for Automation using JavaStream
+        WebElement prod = products.stream()
+                .filter(product->product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3"))
+                .findFirst()
+                .orElse(null);
+
+        prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+        //ng-animating
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+        driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+    }
+}
